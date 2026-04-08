5decfe96d20df63298b6a8e02279215af0551038
NIKOLA PAPRATOVIC <nikola.papratovic@gmail.com>
2026-04-08 14:23:12 +0200

    Clarify comment on method signature promotion

--- UserController.php
+++ UserController.php
@@ -7,7 +7,7 @@ public function store(CreateUserRequest $request, UserService $service)
     // Create a validated, immutable data object
     $userData = CreateUserData::fromRequest($request);
 
-    // other way is to use method signature promotion - in store method add DTO as signature
+    // other way is to use type hinting it as a method signature - in store method add DTO as signature
     // This means that data enters controller, goes to form request then goes to action which calls DTO which is reponsible for making data immutable in the end controller should just return response 
 
     // Pass the DTO to your service
